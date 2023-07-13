7f2a3941964781d6f1e6ed840be2d4644882e10e
Loyda <palacioloydita@gmail.com>
2023-07-13 07:37:19 -0700

    Create a function that genereates a computer choice

--- index.js
+++ index.js
@@ -9,4 +9,15 @@ const userChoice = userInput => {
     }
 }
 
+const computerChoice = () => {
+    let randomNumber = Math.floor(Math.random() *3)
+    
+    if (randomNumber === 0){
+        return "rock"
+    } else if(randomNumber === 1) {
+        return "paper"
+    } else {
+        return "scissors"
+    }
+}
 
